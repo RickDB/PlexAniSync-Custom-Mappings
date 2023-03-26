7b7ade41a4f8a1a497cb99a2cb0536e03c847bc1
Gabriel Nadolny <39236516+InteractiveNinja@users.noreply.github.com>
2023-03-26 16:08:13 +0200

    Script for sorting yaml files by title key (#47)

---
+++ sort.py
@@ -0,0 +1,22 @@
+import glob
+import logging
+
+from ruamel.yaml import YAML
+
+logger = logging.getLogger("PlexAniSync")
+
+yaml = YAML()
+yaml.default_flow_style = False
+yaml.sort_base_mapping_type_on_output = False
+yaml.preserve_quotes = True
+yaml.indent(sequence=4, offset=2)
+yaml.width = 1024
+
+for file in glob.glob("*.yaml"):
+    with open(file, 'r+', encoding='utf-8') as f:
+        file_mappings = yaml.load(f)
+        file_mappings["entries"].sort(key=lambda entry: entry["title"].lower())
+        # Jump to beginning of file and overwrite with sorted entries
+        f.seek(0)
+        yaml.dump(file_mappings, f)
+        f.truncate()
