b83ad85dfffcf16247d96e96a01ac9d6e56c9aa1
Soitora <simon.mattila@protonmail.com>
2025-02-02 15:51:44 +0100

    Log successful validations and sortings
    No output in the GitHub action logs, logging successful runs ensures that you can rely on it actually running

--- validate.py
+++ validate.py
@@ -8,7 +8,13 @@ from jsonschema.exceptions import ValidationError
 from ruamel.yaml import YAML
 import requests
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s"
+)
 logger = logging.getLogger("PlexAniSync")
+
 yaml = YAML(typ="safe")
 SUCCESS = True
 schema_url = "https://raw.githubusercontent.com/RickDB/PlexAniSync/master/custom_mappings_schema.json"
@@ -24,10 +30,12 @@ except FileNotFoundError:
         response = requests.get(schema_url)
         response.raise_for_status()
         schema = response.json()
+        logger.info(f"Successfully fetched schema from {schema_url}")
 
         # Save the fetched schema locally
         with open(local_schema_path, "w", encoding="utf-8") as f:
             json.dump(schema, f, indent=4)
+        logger.info(f"Successfully saved schema to {local_schema_path}")
     except requests.RequestException as e:
         logger.error(f"Failed to fetch schema from {schema_url}: {e}")
         sys.exit(1)
@@ -52,6 +60,7 @@ for file in glob.glob("*.yaml"):
                 raise ValidationError(f"{title} is already mapped", instance=entry)
 
             titles.append(title.lower())
+        logger.info(f"Custom Mappings validation successful for {file}")
     except ValidationError as e:
         logger.error(f"Custom Mappings validation failed for {file}!")
         logger.error(f"{e.message} at entry {e.instance}")
